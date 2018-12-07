397763a33a9b1b1ef94da0bcc9eff0e34a8d4845
Eiliv Aas <eiliv.aas@rikstv.no>
2018-12-08 00:58:47 +0100

    Day 4 - strategy 1

---
+++ day4/strategy1.py
@@ -0,0 +1,52 @@
+import re
+import numpy as np
+from collections import defaultdict
+
+records_test = """[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up"""
+records = records_test.split('\n')
+
+with open('input.txt') as f:
+    records = f.readlines()
+
+
+def half_parse(rec):
+    dt, rest = rec.split(']')
+    date_year, time = dt.split(' ')
+    date = date_year[-5:]
+    minutes = time[-2:]
+
+    return date, int(minutes), rest
+
+
+guard_sleep_time = {}
+for r in sorted(records):
+    date, minutes, rest = half_parse(r)
+    if rest.find('Guard') != -1:
+        guard = re.findall(r'\d+', rest)[0]
+    if rest.find('falls asleep') != -1:
+        asleep = minutes
+    if rest.find('wakes up') != -1:
+        if guard not in guard_sleep_time:
+            guard_sleep_time[guard] = np.zeros(65)
+        for m in range(asleep, minutes):
+            guard_sleep_time[guard][m] += 1
+
+most_sleep = sorted(list(guard_sleep_time.items()), key=lambda _: np.sum(_[1]), reverse=True)[0]
+most_asleep_minute = np.argmax(most_sleep[1])
+print(int(most_sleep[0]) * most_asleep_minute)
