5e7cf7d1061fe3d278d8e7bd3b81fc847bcf5abd
eiliven <publiceiliv@gmail.com>
2018-12-07 23:41:17 +0100

    Day 3 - switching computer, numpy is being difficult

---
+++ day3/overlapping_claims.py
@@ -0,0 +1,38 @@
+import numpy as np
+from collections import namedtuple
+
+# test
+claims = [
+ '#1 @ 1,3: 4x4',
+ '#2 @ 3,1: 4x4',
+ '#3 @ 5,5: 2x2']
+
+Area = namedtuple('Area', ['x', 'y', 'w', 'h'])
+
+
+def parse_claim(c):
+    ignore, b = c.split('@')
+    x_y, w_h = b.split(':')
+    x, y = x_y.strip().split(',')
+    w, h = w_h.strip().split('x')
+    return Area(int(x), int(y), int(w), int(h))
+
+
+# with open('input.txt') as f:
+#     claims = f.readlines()
+
+max_x = max_y = 0
+areas = []
+for claim in claims:
+    a = parse_claim(claim)
+    max_x = max(max_x, a.x + a.w)
+    max_y = max(max_y, a.y + a.h)
+    areas.append(a)
+
+print(max_x, max_y)
+print(areas)
+
+for a in areas:
+    pass
+    # mz = np.zeros(max_x, max_y)
+
